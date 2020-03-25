c96a6ef552084950a28a89427d82080ffa0af0e3
Cole Johnson <cburnjohnson@gmail.com>
2020-03-24 20:44:13 -0700

    styling

--- main.js
+++ main.js
@@ -5,12 +5,10 @@ generateMemeBtn.addEventListener('click', generateMeme);
 
 async function generateMeme() {
     try {
-        const res = await fetch(
-            'https://cors-anywhere.herokuapp.com/https://meme-api.herokuapp.com/gimme'
-        );
+        const res = await fetch('https://meme-api.herokuapp.com/gimme/1');
         const data = await res.json();
 
-        meme.src = data.url;
+        meme.src = data.memes[0].url;
     } catch (err) {
         console.log(err);
     }
